c8853833e0c24039b5d13165cb10bd6d42de7278
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-19 16:42:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 var recipes = {};
 
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
+  1) Objects updateObjectWithKeyAndValue(object, key, value) returns an object with the orignal key value pairs and the new key value pair:
+     ReferenceError: updateObjectWithKeyAndValue is not defined
+      at Context.<anonymous> (test/index-test.js:12:7)
