d4ede3d76c190b5c95b81c687c8961afbe232a7e
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-19 16:51:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,10 @@ function destructivelyDeleteFromObjectByKey(object, key) {
   delete recipes[key];
   return recipes;
 }
+
+  1) Objects destructivelyDeleteFromObjectByKey(object, key) modifies the original object:
+     Error: Expected 1 to be undefined
+      at assert (node_modules/expect/lib/assert.js:29:9)
+      at Expectation.toBe (node_modules/expect/lib/Expectation.js:66:28)
+      at Context.<anonymous> (test/index-test.js:71:27)
+
