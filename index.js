c4890d03422be40d5d50edd183a515fa43b1f754
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-19 16:43:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,3 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, obj, { [key]: value });
 }
 
-  1) Objects updateObjectWithKeyAndValue(object, key, value) returns an object with the orignal key value pairs and the new key value pair:
-     ReferenceError: updateObjectWithKeyAndValue is not defined
-      at Context.<anonymous> (test/index-test.js:12:7)
