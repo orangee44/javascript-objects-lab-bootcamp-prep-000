40f5316077af030778323feee2f952ae9ee89296
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-19 16:45:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value });
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
+  1) Objects destructivelyUpdateObjectWithKeyAndValue(object, key, value) updates `object` with the given `key` and `value` (it is destructive) and returns theentire updated object:
+     ReferenceError: destructivelyUpdateObjectWithKeyAndValue is not defined
+      at Context.<anonymous> (test/index-test.js:31:7)
